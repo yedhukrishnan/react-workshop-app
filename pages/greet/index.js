f67edea1df91e1ab2c3f34938c78f83394be9535
Yedhu Krishnan <yedhukrishnan@yahoo.com>
2023-05-07 00:29:33 +0530

    Add basic styling

--- pages/greet/index.js
+++ pages/greet/index.js
@@ -7,7 +7,17 @@ export default function FirstPost() {
   const name = query.name;
 
   if (name) {
-    return <h1>Hello, {name}</h1>;
+    return (
+      <h1
+        style={{
+          textAlign: "center",
+          background: "#ffdfd2",
+          padding: "20px 10px",
+        }}
+      >
+        Hello, {name}
+      </h1>
+    );
   } else {
     return <h1>Please tell us your name</h1>;
   }
