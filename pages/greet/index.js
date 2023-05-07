0a999a0f463dc2a9916320e3bb2f1577d67d0b2a
Yedhu Krishnan <yedhukrishnan@yahoo.com>
2023-05-07 13:34:17 +0530

    Use an input box to enter name and persist that in local storage

--- pages/greet/index.js
+++ pages/greet/index.js
@@ -5,25 +5,40 @@ export default function Greet() {
   const router = useRouter();
   const { query } = router;
 
-  const [name, setName] = useState(query.name);
+  const [name, setName] = useState("");
 
   useEffect(() => {
-    if (name) {
-      window.localStorage.setItem("name", name);
-    } else {
-      setName(window.localStorage.getItem("name"));
-    }
+    setName(window.localStorage.getItem("name"));
   }, []);
 
+  useEffect(() => {
+    window.localStorage.setItem("name", name);
+  }, [name]);
+
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
+
   return (
-    <h1
-      style={{
-        textAlign: "center",
-        background: "#ffdfd2",
-        padding: "20px 10px",
-      }}
-    >
-      {name ? `Hello, ${name}` : "Please tell us your name"}
-    </h1>
+    <>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        onChange={handleChange}
+        value={name}
+        placeholder="Enter your name"
+      />
+
+      <h1
+        style={{
+          textAlign: "center",
+          background: "#ffdfd2",
+          padding: "20px 10px",
+        }}
+      >
+        {name ? `Hello, ${name}` : "Please tell us your name"}
+      </h1>
+    </>
   );
 }
