d35afa1c5d600fa4673118775cc2f09692d94699
Yedhu Krishnan <yedhukrishnan@yahoo.com>
2023-05-13 17:51:01 +0530

    Move database functions to the lib directory

--- pages/student/index.js
+++ pages/student/index.js
@@ -1,18 +1,5 @@
 import { useState, useEffect } from "react";
-import { createClient } from "@supabase/supabase-js";
-
-const TABLE_NAME = "marks";
-
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
-
-export const read = async (studentId) =>
-  await supabase.from(TABLE_NAME).select().eq("student_id", studentId).single();
-
-export const insert = (data) =>
-  supabase.from(TABLE_NAME).insert(data).select().single();
+import { read } from "@/lib/database";
 
 export default function Student() {
   const [studentId, setStudentId] = useState("");
